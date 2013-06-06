4b5824d7f6feb44f2144820c437114130b6ff8b4
John Hedges <john@drystone.co.uk>
2013-06-06 21:22:57 +0100

    watches overrides.xml to reload config

--- Main.hs
+++ Main.hs
@@ -4,10 +4,11 @@ import System.Console.GetOpt (OptDescr(Option), ArgDescr(ReqArg, NoArg), usageIn
 import System.Environment (getProgName, getArgs)
 import System.Exit (exitSuccess)
 import Data.List (intercalate)
-import Control.Concurrent (threadDelay)
+import Control.Concurrent (threadDelay, forkIO, putMVar, tryTakeMVar, newEmptyMVar)
 import Control.Monad (forever)
 import System.Directory (renameFile)
 import qualified Data.ByteString.Char8 as BS
+import System.INotify (initINotify, addWatch, EventVariety(..))
 
 import Thermometer
 import Zone
@@ -98,33 +99,43 @@ main = do
     args <- getArgs
     let (actions, nonOptions, errors) = getOpt RequireOrder options args
     opts <- foldl (>>=) (return startOptions) actions
-    let Options { optConfigDir  = configDir
-                , optLibDir     = libDir
-                , optRunDir     = runDir
-                , optArexxDir   = arexxDir
-                , optOwDir      = owDir
-                , optUdinDir    = udinDir
-                , optFht8vDir   = fht8vDir } = opts
-
-    zones <- loadZones (configDir ++ "/zones.xml")
-    routines <- loadRoutines (configDir ++ "/routines.xml") zones
-    overrides <- loadOverrides (libDir ++ "/overrides.xml") zones
-    thermometers <- loadThermometers (configDir ++ "/thermometers.xml") owDir arexxDir
-    thermostats <- loadThermostats (configDir ++ "/thermostats.xml") routines overrides
-    controls <- loadControls (configDir ++ "/controls.xml") udinDir fht8vDir
-    forever (loop thermometers thermostats controls runDir)
+    mvar <- newEmptyMVar
+    inotify <- initINotify
+    addWatch inotify [MoveIn] (optLibDir opts) $ \e -> putMVar mvar e
+    daemon opts mvar
   where
-    loop thermometers thermostats controls runDir = do
-        temperatures <- readThermometers thermometers
-        saveState runDir "temperatures.xml" $ getTemperatureXml temperatures
-        thermostatStates <- testThermostats temperatures thermostats
-        saveState runDir "state.xml" $ getThermostatStateXml thermostatStates
-        controlStates <- evalControlConditions thermostatStates controls
-        saveState runDir "control-state.xml" $ getControlStateXml controlStates
-        actuateControls controlStates controls
-        threadDelay 5000000
-        return ()
+    daemon opts mvar = do
+        let Options { optConfigDir  = configDir
+                    , optLibDir     = libDir
+                    , optRunDir     = runDir
+                    , optArexxDir   = arexxDir
+                    , optOwDir      = owDir
+                    , optUdinDir    = udinDir
+                    , optFht8vDir   = fht8vDir } = opts
+        putStrLn "(Re)loading configuration"
+        zones <- loadZones (configDir ++ "/zones.xml")
+        routines <- loadRoutines (configDir ++ "/routines.xml") zones
+        overrides <- loadOverrides (libDir ++ "/overrides.xml") zones
+        thermometers <- loadThermometers (configDir ++ "/thermometers.xml") owDir arexxDir
+        thermostats <- loadThermostats (configDir ++ "/thermostats.xml") routines overrides
+        controls <- loadControls (configDir ++ "/controls.xml") udinDir fht8vDir
+        loop thermometers thermostats controls runDir
+        daemon opts mvar
+      where
+        loop thermometers thermostats controls runDir = do
+            temperatures <- readThermometers thermometers
+            saveState runDir "temperatures.xml" $ getTemperatureXml temperatures
+            thermostatStates <- testThermostats temperatures thermostats
+            saveState runDir "state.xml" $ getThermostatStateXml thermostatStates
+            controlStates <- evalControlConditions thermostatStates controls
+            saveState runDir "control-state.xml" $ getControlStateXml controlStates
+            actuateControls controlStates controls
+            reload <- tryTakeMVar mvar
+            case reload of
+                Nothing -> threadDelay (5*10^6) >> loop thermometers thermostats controls runDir
+                otherwise -> return ()
 
+-- write to a temporary file and rename into place
 saveState :: FilePath -> FilePath -> String -> IO ()
 saveState dir fnam s = do
     BS.writeFile tnam (BS.pack s)
