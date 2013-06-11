1678622930eccf3be4ada9e111dd6ccce8611c16
John Hedges <john@drystone.co.uk>
2013-06-11 13:04:54 +0100

    Adds before and after tags for controls.xml

--- Control.hs
+++ Control.hs
@@ -12,7 +12,7 @@ import Data.List (find)
 import qualified Data.ByteString.Char8 as BS
 import Control.Monad (mapM_, foldM)
 import Data.Time.Calendar (Day(ModifiedJulianDay))
-import Data.Time.Clock (UTCTime(UTCTime, utctDay, utctDayTime), diffUTCTime, getCurrentTime)
+import Data.Time.Clock (UTCTime(UTCTime, utctDay, utctDayTime), diffUTCTime, getCurrentTime, NominalDiffTime, addUTCTime)
 
 import Thermostat
 import Xml
@@ -35,7 +35,8 @@ data ControlCondition = ControlConditionNot ControlCondition
                       | ControlConditionOr [ControlCondition]
                       | ControlConditionAnd [ControlCondition]
                       | ControlConditionOver ThermostatId
-                      | ControlConditionOn Control
+                      | ControlConditionOn ControlId
+                      | ControlConditionBefore ControlId NominalDiffTime
                       | NoControlCondition
     deriving (Show)
 
@@ -57,15 +58,17 @@ loadControls file udinDir fht8vDir = do
             , controlCondition  = extractCondition rootEl $ head $ X.elChildren el }
 
         extractCondition rootEl el = case X.qName $ X.elName el of
-            "not"   -> ControlConditionNot $ extractCondition rootEl (head $ X.elChildren el)
-            "or"    -> ControlConditionOr $ map (extractCondition rootEl) (X.elChildren el)
-            "and"   -> ControlConditionAnd $ map (extractCondition rootEl) (X.elChildren el)
-            "over"  -> ControlConditionOver $ attr "thermostat-id" el
-            "under" -> ControlConditionNot $ ControlConditionOver $ attr "thermostat-id" el
-            "on"    -> ControlConditionOn $ extractControl rootEl $ findControlEl (attr "control-id" el) rootEl
-            "off"   -> ControlConditionNot $ ControlConditionOn $ extractControl rootEl $ findControlEl (attr "control-id" el) rootEl
-            "true"  -> extractMacro rootEl $ findMacroEl (attr "macro-id" el) rootEl
-            "false" -> ControlConditionNot $ extractMacro rootEl $ findMacroEl (attr "macro-id" el) rootEl
+            "not"     -> ControlConditionNot $ extractCondition rootEl (head $ X.elChildren el)
+            "or"      -> ControlConditionOr $ map (extractCondition rootEl) (X.elChildren el)
+            "and"     -> ControlConditionAnd $ map (extractCondition rootEl) (X.elChildren el)
+            "over"    -> ControlConditionOver $ attr "thermostat-id" el
+            "under"   -> ControlConditionNot $ ControlConditionOver $ attr "thermostat-id" el
+            "on"      -> ControlConditionOn $ attr "control-id" el
+            "off"     -> ControlConditionNot $ ControlConditionOn $ attr "control-id" el
+            "true"    -> extractMacro rootEl $ findMacroEl (attr "macro-id" el) rootEl
+            "false"   -> ControlConditionNot $ extractMacro rootEl $ findMacroEl (attr "macro-id" el) rootEl
+            "before"  -> ControlConditionBefore (attr "control-id" el) (fromIntegral $ read $ attr "timeout" el)
+            "after"   -> ControlConditionNot $ ControlConditionBefore (attr "control-id" el) (fromIntegral $ read $ attr "timeout" el)
 
         extractMacro rootEl el = extractCondition rootEl (head $ X.elChildren el)
 
@@ -79,27 +82,29 @@ loadControls file udinDir fht8vDir = do
                 Nothing  -> error "Cannot use fht8v device without specifying fht8v mountpoint with -f"
 
         findControlEls = X.filterChildren (\e -> X.qName (X.elName e) == "control") 
-        findControlEl id rootEl = fromJust $ find (\e -> attr "id" e == id) $ findControlEls rootEl
 
         findMacroEls = X.filterChildren (\e -> X.qName (X.elName e) == "macro") 
         findMacroEl id rootEl = fromJust $ find (\e -> attr "id" e == id) $ findMacroEls rootEl
 
 
 evalControlConditions :: Map.Map ThermostatId Bool -> [Control] -> IO ([Control])
-evalControlConditions thermostatStates = mapM ecc
-  where
+evalControlConditions thermostatStates controls = mapM ecc controls
+  where 
     ecc c = do 
         bState <- evalCondition (controlCondition c)
         let newState = if bState then ControlStateOn else ControlStateOff
         ct <- getCurrentTime
         return $ c { controlState    = newState
                    , controlChange   = if newState /= controlState c then ct else controlChange c }
-      where
     evalCondition (ControlConditionNot c) = fmap not (evalCondition c)
     evalCondition (ControlConditionOr cs) = fmap (True `elem`) (mapM evalCondition cs)
     evalCondition (ControlConditionAnd cs) = fmap (not . (False `elem`)) (mapM evalCondition cs)
-    evalCondition (ControlConditionOver t) = return $ fromJust $ Map.lookup t thermostatStates
-    evalCondition (ControlConditionOn c) = return $ controlState c == ControlStateOn
+    evalCondition (ControlConditionOver tid) = return $ fromJust $ Map.lookup tid thermostatStates
+    evalCondition (ControlConditionOn cid) = return $ controlState (fromJust $ find (\c -> controlId c == cid) controls) == ControlStateOn
+    evalCondition (ControlConditionBefore cid d) = do
+        let c = fromJust $ find (\c -> controlId c == cid) controls
+        ct <- getCurrentTime
+        return $ ct <= addUTCTime d (controlChange c)
 
 actuateControls :: [Control] -> IO ()
 actuateControls = mapM_ actuate
